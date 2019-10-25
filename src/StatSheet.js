74e93a34429e2529f99b1e49c5ce41af1a269ca2
James Armbrust <james.armbrust@gmail.com>
2019-10-25 08:41:32 -0600

    some cleanup and a bug fix, WIP

--- src/StatSheet.js
+++ src/StatSheet.js
@@ -24,29 +24,25 @@ class StatSheet extends Component {
     this.setState({ [ability]: event });
   }
 
-  // TODO duplicated code, put in a loop
   renderDropdown(ability) {
-    return (<DropdownButton size="sm" id="dropdown-ability-score" title="Score" variant="secondary">
-        <Dropdown.Item eventKey="0" onSelect={(e) => this.onClickHandler(ability, e)}>0</Dropdown.Item>
-        <Dropdown.Item eventKey="1" onSelect={(e) => this.onClickHandler(ability, e)}>1</Dropdown.Item>
-        <Dropdown.Item eventKey="2" onSelect={(e) => this.onClickHandler(ability, e)}>2</Dropdown.Item>
-        <Dropdown.Item eventKey="3" onSelect={(e) => this.onClickHandler(ability, e)}>3</Dropdown.Item>
-        <Dropdown.Item eventKey="4" onSelect={(e) => this.onClickHandler(ability, e)}>4</Dropdown.Item>
-        <Dropdown.Item eventKey="5" onSelect={(e) => this.onClickHandler(ability, e)}>5</Dropdown.Item>
-        <Dropdown.Item eventKey="7" onSelect={(e) => this.onClickHandler(ability, e)}>7</Dropdown.Item>
-        <Dropdown.Item eventKey="9" onSelect={(e) => this.onClickHandler(ability, e)}>9</Dropdown.Item>
-      </DropdownButton>);
+    const values = ['1', '2', '3', '4', '5', '7', '9'];
+    const dropdownValues = values.map(val => <Dropdown.Item eventKey={val} onSelect={(e) => this.onClickHandler(ability, e)}>{val}</Dropdown.Item>);
+    return (
+      <DropdownButton size="sm" id="dropdown-ability-score" title="Score" variant="secondary">
+        {dropdownValues}
+      </DropdownButton>
+    );
   }
 
   render() {
     return ( 
       <div>
-        <div><span className='str'>{this.renderDropdown('str')}</span> <span> Strength: <AbilityScore score={this.state.str} /></span></div>
-        <div><span className='dex'>{this.renderDropdown('dex')}</span> <span> Dexterity: <AbilityScore score={this.state.dex} /></span></div>
-        <div><span className='con'>{this.renderDropdown('con')}</span> <span> Constitution: <AbilityScore score={this.state.con} /></span></div>
-        <div><span className='int'>{this.renderDropdown('int')}</span> <span> Intelligence: <AbilityScore score={this.state.int} /></span></div>
-        <div><span className='wis'>{this.renderDropdown('wis')}</span> <span> Wisdom: <AbilityScore score={this.state.wis} /></span></div>
-        <div><span className='chr'>{this.renderDropdown('chr')}</span> <span> Charisma: <AbilityScore score={this.state.chr} /></span></div>
+        <div><span className='str div-inline'>{this.renderDropdown('str')}</span> <span> Strength: <AbilityScore score={this.state.str} /></span></div>
+        <div><span className='dex div-inline'>{this.renderDropdown('dex')}</span> <span> Dexterity: <AbilityScore score={this.state.dex} /></span></div>
+        <div><span className='con div-inline'>{this.renderDropdown('con')}</span> <span> Constitution: <AbilityScore score={this.state.con} /></span></div>
+        <div><span className='int div-inline'>{this.renderDropdown('int')}</span> <span> Intelligence: <AbilityScore score={this.state.int} /></span></div>
+        <div><span className='wis div-inline'>{this.renderDropdown('wis')}</span> <span> Wisdom: <AbilityScore score={this.state.wis} /></span></div>
+        <div><span className='chr div-inline'>{this.renderDropdown('chr')}</span> <span> Charisma: <AbilityScore score={this.state.chr} /></span></div>
       </div>
     );
   }
